11094f36ac4f94d46299ad6bd54c81dc4e9eff65
vuvivian <1031355459@qq.com>
2020-04-10 00:07:23 +0800

    :wrench:

--- client-app/src/components/global/v-upload/index.js
+++ client-app/src/components/global/v-upload/index.js
@@ -1,2 +1,10 @@
+/*
+ * @Author: vivian
+ * @Date: 2020-04-09 23:39:01
+ * @LastEditTime: 2020-04-09 23:59:39
+ * @LastEditors: 上传组件封装
+ * @Description: In User Settings Edit
+ * @FilePath: /vue-node-mongo/client-app/src/components/global/v-upload/index.js
+ */
 import Main from './main.vue'
 export default Main
--- client-app/src/components/global/v-upload/main.vue
+++ client-app/src/components/global/v-upload/main.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: vuvivia
+ * @Date: 2020-04-09 23:39:01
+ * @LastEditTime: 2020-04-10 00:04:15
+ * @LastEditors: Please set LastEditors
+ * @Description: 上传组件封装
+ * @FilePath: /vue-node-mongo/client-app/src/components/global/v-upload/main.vue
+ -->
 <template>
   <div class="v-upload">
     v-upload组件
@@ -8,7 +16,7 @@ export default {
   name: 'v-upload'
 }
 </script>
-<style lang="scss" scoped>
+<style lang="less" scoped>
 .v-upload {
 
 }
