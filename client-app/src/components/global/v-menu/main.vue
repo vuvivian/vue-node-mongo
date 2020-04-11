1cd0f55aa5d88103d034579a883e5c8716884923
vuvivian <1031355459@qq.com>
2020-04-11 23:34:08 +0800

    :rocket: 配置mock

---
+++ client-app/src/components/global/v-menu/main.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: vuvivian
+ * @Date: 2020-04-11 18:05:31
+ * @LastEditTime: 2020-04-11 18:06:18
+ * @LastEditors: Please set LastEditors
+ * @Description: 可模糊搜索的菜单组件
+ * @FilePath: /vue-node-mongo/client-app/src/components/global/v-menu/main.vue
+ -->
+<template>
+  <div class="v-menu">
+    v-menu组件
+  </div>
+</template>
+<script>
+export default {
+  name: 'v-menu'
+}
+</script>
+<style lang="scss" scoped>
+.v-menu {
+
+}
+</style>
--- client-app/vue.config.js
+++ client-app/vue.config.js
@@ -1,13 +1,14 @@
 /*
  * @Author: vuvivian
  * @Date: 2020-04-11 17:05:02
- * @LastEditTime: 2020-04-11 17:12:12
+ * @LastEditTime: 2020-04-11 23:20:00
  * @LastEditors: Please set LastEditors
  * @Description: 配置文件
  * @FilePath: /vue-node-mongo/client-app/vue.config.js
  */
 const path = require("path");
 const axios = require('axios');
+const mockIndexData = require("./mock/index.json");
 
 const resolve = dir => {
   return path.join(__dirname, dir);
@@ -24,4 +25,18 @@ module.exports = {
       .set("@assets", resolve("src/assets"))
       .set("@scripts", resolve("src/assets/js"));
   },
+  devServer: {
+    port: 8080,
+    disableHostCheck: true,
+    hotOnly: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/mock'
+        }
+      }
+    }
+  }
 }
